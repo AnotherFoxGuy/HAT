d7328bc182d9015647c41a751467cc68c6e281f5
AnotherFoxGuy <haselnut116601@gmail.com>
2015-01-05 13:30:53 +0100

    Added a player-model

--- Assets/Scripts/Player.js
+++ Assets/Scripts/Player.js
@@ -1,30 +1,52 @@
 ﻿#pragma strict
 
 private var PlayerRigidbody : Rigidbody ;
+private var ConstructionWorker : GameObject ;
 private var HitPoint: RaycastHit;
-var MaxSpeed = 7;
+private var LastClip: String;
+public var MaxSpeed = 7;
 
 function Start () {
 	PlayerRigidbody = this.GetComponent(Rigidbody);
+	ConstructionWorker = GameObject.Find("ConstructionWorker");
+	ConstructionWorker.transform.eulerAngles.y = 90;
 }
 
 function Update () {
-	
 	if(Input.GetKey("d")){
-		if(PlayerRigidbody.velocity.x < MaxSpeed){
-			PlayerRigidbody.AddForce(transform.TransformDirection(Vector3.right*20));	
-		}
+	ConstructionWorker.transform.eulerAngles.y = 90;
+		if(PlayerRigidbody.velocity.x < MaxSpeed)
+			PlayerRigidbody.AddForce(transform.TransformDirection(Vector3.right*20));
+	}
+	if(Input.GetKey("a")){
+	ConstructionWorker.transform.eulerAngles.y = -90;
+		if(PlayerRigidbody.velocity.x > -MaxSpeed)
+			PlayerRigidbody.AddForce(transform.TransformDirection(Vector3.left*20));
+	}
+	if(Input.GetKeyDown("space")){
+		if(Physics.Raycast(this.transform.position, Vector3.down, 1))
+			PlayerRigidbody.AddForce(transform.TransformDirection(Vector3.up*400));
 	}
 
-	if(Input.GetKey("a")){
-		if(PlayerRigidbody.velocity.x > -MaxSpeed){
-			PlayerRigidbody.AddForce(transform.TransformDirection(Vector3.left*20));
-		}
+	//==============================================================================================
+	if (Physics.Raycast(this.transform.position, Vector3.down, 1)){
+		AnimateThis("idle" , false);
+		if(Input.GetKey("d"))
+			AnimateThis("run" , false);
+		else if(Input.GetKey("a"))
+			AnimateThis("run" , false);
 	}
-		
-	if(Input.GetKeyDown("space")){
-		if(Physics.Raycast(this.transform.position, Vector3.down, 0.6)){
-			PlayerRigidbody.AddForce(transform.TransformDirection(Vector3.up*400));
-		}
+	else if (!Physics.Raycast(this.transform.position, Vector3.down, 1)){
+		AnimateThis("jump_pose" , true);
+	}
+
+}
+function AnimateThis(CurrentClip : String , Forced : boolean) {
+	if (LastClip != CurrentClip){
+		if(Forced)
+			ConstructionWorker.animation.Play(CurrentClip);
+		else
+			ConstructionWorker.animation.CrossFade(CurrentClip, 0.2);
+		LastClip = CurrentClip;
 	}
 }
